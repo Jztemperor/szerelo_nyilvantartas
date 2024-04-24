a087dcee0ec9189c403f05c50a73fb6d58f6bfa2
Daniel Mozsar <jezzy@pop-os.t.hu>
2024-04-24 21:45:47 +0200

    index for workorders done

--- app/Http/Controllers/WorksheetsController.php
+++ app/Http/Controllers/WorksheetsController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Owner;
+use App\Models\WorkOrder;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class WorksheetsController extends Controller
 {
+    /*
+
     public function index() : View
     {
         return view('pages.worksheets',[
@@ -16,6 +19,19 @@ class WorksheetsController extends Controller
             'owners' => Owner::orderBy('created_at','DESC')->paginate(5)
         ]);
     }
+
+    */
+
+    public function index() : View
+    {
+        $workorders = WorkOrder::paginate(10);
+
+        return view('pages.worksheets', [
+            'titles' => ['Worksheets'],
+            'workorders'=> $workorders
+        ]);
+    }
+
     public function show(Request $request, $id) : View
     {
         return view('contents.view_worksheet',[
--- database/factories/WorkOrderFactory.php
+++ database/factories/WorkOrderFactory.php
@@ -17,7 +17,7 @@ class WorkOrderFactory extends Factory
     public function definition(): array
     {
         return [
-            'status' => fake()->boolean()
+            'status' => fake()->randomElement(['working', 'finished', 'starting', 'closed'])
         ];
     }
 }
