7f50184563998aa10d441b56f609ea8860c7ba7d
Jztemperor <daniel.mozsar01@gmail.com>
2024-05-19 23:29:51 +0200

    Worksheet is now exportable

--- resources/views/contents/worksheets/show.blade.php
+++ resources/views/contents/worksheets/show.blade.php
@@ -1,8 +1,11 @@
 @extends('layout.layout')
 
-<div class="p-4 sm:ml-64">
-    <div class="p-4  mt-14">
+<div class="p-4 sm:ml-64 print:p-0 print:ml-0 print:w-full">
+    <div class="p-4 mt-14 print:p-0 print:mt-0 print:w-full">
+        <div class="flex w-full justify-between">
         @include('includes._breadcrumb')
+        <button onclick="printPage()" class="print:hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Export</button>
+        </div>
 
         @include('includes._worksheet', [
             'administrator' => $operator,
@@ -17,3 +20,9 @@
         ])
     </div>
 </div>
+
+<script>
+    const printPage = () => {
+        print();
+    }
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
         "./node_modules/flowbite/**/*.js"
     ],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                print: {raw: 'print'},
+                screen: {raw: 'screen'}
+            }
+        },
     },
     plugins: [
         require('flowbite/plugin')
